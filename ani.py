bfeaad40bb867b52e99b58b74855e1e84d202f63
LIUXIN <liuxin.bit@gmail.com>
2017-07-24 22:51:47 -0400

    ALS

---
+++ ani.py
@@ -0,0 +1,34 @@
+import imageio
+imageio.plugins.ffmpeg.download()
+import moviepy.editor as mpy
+
+import numpy as np
+import gizeh as gz
+
+
+Width,Height,Length = 1920,1080,120
+widthNumber = Width / Length
+heightNumber = Height / Length
+Duration = 1
+
+def make_frame(t):
+    surface = gz.Surface(Width,Height)
+    center = []
+    T = round(t*widthNumber*heightNumber/Duration)
+
+    x = ( T%widthNumber )*Length + Length/2
+    y = ( T//widthNumber )*Length + Length/2
+    
+    # print t, T, x, y
+
+    square = gz.square(l=Length, xy=[x,y], fill=(0, 0, 0))
+    square.draw(surface)
+
+    return surface.get_npimage()
+
+clip = mpy.VideoClip(make_frame, duration=Duration)
+clip.write_gif("background.gif",fps=widthNumber*heightNumber, opt="OptimizePlus")
+
+# t = 0.0933333333333
+# y = (t * 12 %% 6) 
+# print 3%2
